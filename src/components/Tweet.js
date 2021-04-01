3f319aa860e55fb005cad7e17b3c1436330db68c
Tyago Nunes <tyagonunes@gmail.com>
2021-03-31 21:31:07 -0300

    complete tweet component

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { formatTweet } from '../utils/helpers'
+import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline } from 'react-icons/ti' 
 import { TiHeartOutline } from 'react-icons/ti' 
 import { TiHeartFullOutline } from 'react-icons/ti'
 
 function Tweet ({ tweet }) {
+
+  function handleLike (e) {
+    e.preventDefault()
+
+    // todo: Handle Like Tweet
+  }
+  function toParent(e, id) {
+    e.preventDefault()
+  }
   
   if(tweet === null) {
     return <p>This tweet doesnt exist</p>
@@ -18,10 +27,35 @@ function Tweet ({ tweet }) {
   return (
     <div className="tweet">
       <img
-          src={avatar}
-          alt={`Avatar of ${name}`}
-          className='avatar'
-        />
+        src={avatar}
+        alt={`Avatar of ${name}`}
+        className='avatar'
+      />
+      <div className='tweet-info'>
+        <div>
+          <span>{name}</span>
+          <div>{formatDate(timestamp)}</div>
+          {parent && (
+            <button 
+              className="replying-to"
+              onClick={(e) => toParent(e, parent.id)}
+            >
+              Replying to @{parent.author}
+            </button>
+          )}
+          <p>{text}</p>
+        </div>
+        <div className='tweet-icons'>
+          <TiArrowBackOutline className='tweet-icon' />
+          <span>{replies !== 0 && replies}</span>
+          <button className='heart-button' onClick={handleLike}>
+            {hasLiked === true
+              ? <TiHeartFullOutline color='#e0245e' className='tweet-icon' />
+              : <TiHeartOutline className='tweet-icon'/>}
+          </button>
+          <span>{likes !== 0 && likes}</span>
+        </div>
+      </div>
     </div>
   )
 }
